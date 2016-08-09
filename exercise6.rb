3625dae4f4f424ef9efa7fee02bf9ed863cdcad1
John Howie <jkhowie63@gmail.com>
2016-08-09 16:03:02 -0400

    added rice to list and reprinted with the number of items needed

--- exercise6.rb
+++ exercise6.rb
@@ -1,3 +1,14 @@
 grocery_list = ["carrots", "toilet paper", "apples", "salmon"]
 
-grocery_list.each {|item| puts "*" + item}
+def print_list(items)
+  items.each {|item| puts "*" + item}
+end
+
+
+print_list(grocery_list)
+
+grocery_list << "rice"
+
+print_list(grocery_list)
+
+puts "We need to pickup #{grocery_list.length} items"
