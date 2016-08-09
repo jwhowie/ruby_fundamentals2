c495dc51ebaef67ba4127623af9cab1541aa9ed5
John Howie <jkhowie63@gmail.com>
2016-08-09 16:09:13 -0400

    added bananna check

--- exercise6.rb
+++ exercise6.rb
@@ -12,3 +12,9 @@ grocery_list << "rice"
 print_list(grocery_list)
 
 puts "We need to pickup #{grocery_list.length} items"
+
+if grocery_list.includes?("bananas")
+  puts "You need to pick up bananas"
+else
+  puts "You don't need to pick up bananas today"
+end
