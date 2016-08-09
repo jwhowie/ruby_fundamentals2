4243db9a816c0f70bf8b797c6804ce32b164326d
John Howie <jkhowie63@gmail.com>
2016-08-09 16:17:16 -0400

    sorted list and removed salmon

--- exercise6.rb
+++ exercise6.rb
@@ -13,8 +13,18 @@ print_list(grocery_list)
 
 puts "We need to pickup #{grocery_list.length} items"
 
-if grocery_list.includes?("bananas")
+if grocery_list.include?("bananas")
   puts "You need to pick up bananas"
 else
   puts "You don't need to pick up bananas today"
 end
+
+puts "The second item in the list is #{grocery_list[1]}"
+
+print_list(grocery_list.sort)
+
+puts "Can\'t find salmon removing it from the list"
+
+grocery_list.delete("salmon")
+
+print_list(grocery_list.sort)
