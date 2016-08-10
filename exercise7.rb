d95a30193f5292661656e1c4503a3947754bc831
John Howie <jkhowie63@gmail.com>
2016-08-10 10:16:07 -0400

    %1

--- exercise7.rb
+++ exercise7.rb
@@ -10,7 +10,7 @@ students[:cohort4] = 43
 
 puts students.keys
 
-students.each {|key, value| students[key] = value += (value * 0.05).round(0)}
+students.each {|key, value| students[key] = value += (value * 0.05).round(2)}
 
 students.each { |key, value| puts "#{key}: #{value} students"  }
 
@@ -19,6 +19,7 @@ students.delete(:cohort2)
 students.each { |key, value| puts "#{key}: #{value} students"  }
 
 total_students = 0
+
 students.each {|key, value| total_students += value}
 
 puts "Total students for all cohorts is #{total_students}"
